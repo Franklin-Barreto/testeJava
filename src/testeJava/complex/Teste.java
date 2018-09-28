a3a9a66ea06c85c21489eca954ea1bc8100e4345
Franklin-Barreto <franklin-barreto@hotmail.com>
2018-09-28 17:26:00 -0300

    excluindo linhas comentadas

--- src/testeJava/complex/Teste.java
+++ src/testeJava/complex/Teste.java
@@ -3,11 +3,8 @@ package testeJava.complex;
 public class Teste {
 
 	public static void main(String[] args){
-//		ComplexNumber numero = new ComplexNumber(6, 5);
-	//	ComplexNumber numero = new ComplexNumber();
-		//numero.soma(new ComplexNumber(2, -1));
 		
 		System.out.println(ComplexNumber.somaNumeros(new ComplexNumber(6, 5), new ComplexNumber(2, -1)));
-		//System.out.println(numero);
+
 	}
 }
