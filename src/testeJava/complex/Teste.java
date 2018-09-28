f4638fc3eeea476a46ecff7cbdf8794e4e8ecf9d
Franklin-Barreto <franklin-barreto@hotmail.com>
2018-09-28 17:48:04 -0300

    ComplexNumber finalizado

--- src/testeJava/complex/Teste.java
+++ src/testeJava/complex/Teste.java
@@ -2,8 +2,10 @@ package testeJava.complex;
 
 public class Teste {
 
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+		ComplexNumber number = new ComplexNumber(2, 3);
+		number.soma(new ComplexNumber(5, 4));
+		System.out.println(number);
 		System.out.println(ComplexNumber.somaNumeros(new ComplexNumber(6, 5), new ComplexNumber(2, -1)));
 
 	}
